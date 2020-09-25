69209e03d55ded0fce74f490dff3771c7239f983
aesthetical <57580886+Sxmurai@users.noreply.github.com>
2020-09-24 22:50:04 -0500

    Adds "only toast on ping" setting (#1)
    * add settings
    
    * update manifest with new version
    
    * add some of this shit
    
    * sounds fun doesnt it
    
    * oops
    
    * add fix
    
    * add check to see who was pinged
    
    * okay fr this time, it should work

---
+++ Settings.jsx
@@ -0,0 +1,20 @@
+const { SwitchItem } = require("powercord/components/settings");
+const { React } = require("powercord/webpack");
+
+module.exports = class Settings extends React.Component {
+  render() {
+    const { getSetting, toggleSetting } = this.props;
+
+    return (
+      <div>
+        <SwitchItem
+          note="If to only notify you if you were pinged or not."
+          value={getSetting("notifyPing", false)}
+          onChange={() => toggleSetting("notifyPing")}
+        >
+          Only notify on ping
+        </SwitchItem>
+      </div>
+    );
+  }
+};
