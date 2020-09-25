47d7e035fa10f12dc68265b6b72908fba0a31ab6
aesthetical <57580886+Sxmurai@users.noreply.github.com>
2020-09-24 22:05:22 -0500

    add some of this shit

--- index.js
+++ index.js
@@ -1,79 +1,84 @@
-const { getModule } = require('powercord/webpack');
-const { inject, uninject } = require('powercord/injector');
-const { Plugin } = require('powercord/entities');
+const { getModule } = require("powercord/webpack");
+const { inject, uninject } = require("powercord/injector");
+const { Plugin } = require("powercord/entities");
 
-const getUser = getModule([ 'getCurrentUser' ], false).getUser;
-const getChannel = getModule([ 'getChannel' ], false).getChannel;
-const getGuild = getModule([ 'getGuild' ], false).getGuild;
+const getUser = getModule(["getCurrentUser"], false).getUser;
+const getChannel = getModule(["getChannel"], false).getChannel;
+const getGuild = getModule(["getGuild"], false).getGuild;
 
-module.exports = class InAppNotifs extends Plugin {
-    startPlugin () {
-        try {
-            const showNotifm = getModule([ 'makeTextChatNotification' ], false);
-            const transition = getModule([ 'transitionTo' ], false);
+const Settings = require("./Settings");
 
-            inject('notifthing-yes', showNotifm, 'makeTextChatNotification', (args) => {
-                const toastID = `notif-${Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)}`;
-                const dispTime = Math.min(Math.max(args[1].content.split(' ').length * 0.5e3, 4e3), 10e3);
-                const chanName = args[0].guild_id ? `#${args[0].name}` : 'DM\'s';
-                const transitionString = `/channels/${ args[0].guild_id ? args[0].guild_id : '@me'}/${args[0].id}/${args[1].id}`;
-                const guild = getGuild(args[0].guild_id);
-                const content = /<[@#!&]?[@#!&]?(\d+)>/g.test(args[1].content) ?  this.parse(args[1].content, guild) : args[1].content;
+module.exports = class InAppNotifciations extends Plugin {
+  startPlugin() {
+    powercord.api.settings.registerSettings(`ian-settings`, {
+      category: this.entityID,
+      label: "In App Notifications",
+      render: Settings,
+    });
 
-                powercord.api.notices.sendToast(toastID, 
-                    { 
-                        header: guild ? `${args[2].username} in ${guild.name}` : args[2].username,
-                        content: content,
-                        timeout: dispTime,
-                        buttons: [ {
-                            text: `Jump to ${chanName}`,
-                            look: 'outlined',
-                            size: 'small',
-                            onClick: () => {
-                                transition.transitionTo(transitionString);
-                            }
-                        }, {
-                            text: 'Dismiss',
-                            look: 'ghost',
-                            size: 'small'
-                        } ]
-                    });
-                return args;
-            }, true);
-        } catch (error) {
-            console.error('In App Notifs seems to have broken, please contact the developer.');
-            console.error(error);
-        }
-        
-    }
+    const onPing = this.settings.get("notifyPing", false);
 
-    pluginWillUnload () {
-        uninject('notifthing-yes');
-    }
-    
-    parse(message, guild) {
-        const userRegex = /<@!?(\d+)>/g;
-        const chanRegex = /<#!?(\d+)>/g;
-        const roleRegex = /<@&?(\d+)>/g;
+    try {
+      const show = getModule(["makeTextChatNotification"], false);
+      const transition = getModule(["transitionTo"], false);
 
-        //format user mentions
-        let formattedMessage = message.replace(userRegex, (m, p) => {
-            let username = getUser(p).username;
-            return `@${username}`;
+      inject("ian", show, "makeTextChatNotification", (args, res) => {
+        const toast = (Math.random().toString(36) + Date.now()).substring(2, 7);
+
+        const server = getGuild(args[0].guild_id);
+
+        if (!args[1].content.match(/<(@!?|#|@&)?(\d+)>/g) && onPing) return;
+
+        powercord.api.notices.sendToast(toast, {
+          header: server
+            ? `${args[2].username} in ${server.name}`
+            : args[2].username,
+          timeout: Math.min(
+            Math.max(args[1].content.split(" ").length * 0.5e3, 4e3),
+            10e3
+          ),
+          content: this.parse(args[1].content, server),
+          buttons: [
+            {
+              text: `Jump to ${
+                server ? `#${args[0].name}` : "Direct Messages"
+              }`,
+              look: "outlined",
+              size: "small",
+              onClick: () =>
+                transition.transitionTo(
+                  `/channels/${args[0].guild_id ?? "@me"}/${args[0].id}/${
+                    args[1].id
+                  }`
+                ),
+            },
+
+            {
+              text: "Dismiss",
+              look: "ghost",
+              size: "small",
+            },
+          ],
         });
 
-        //format channel mentions
-        formattedMessage = formattedMessage.replace(chanRegex, (m, p) => {
-            let chanName = getChannel(p).name;
-            return `#${chanName}`;
-        });
-
-        //format role mentions
-        formattedMessage = formattedMessage.replace(roleRegex, (m, p) => {
-            let roleName = guild.roles[p].name;
-            return `@${roleName}`;
-        });
-
-        return formattedMessage
+        return res;
+      });
+    } catch (error) {
+      console.error(
+        `There seems to have been a problem with the in app notifications. Please report this to the developer.\n\n${error}`
+      );
     }
+  }
+
+  pluginWillUnload() {
+    uninject("ian");
+    powercord.api.settings.unregisterSettings(`ian-settings`);
+  }
+
+  parse(content, guild) {
+    return content
+      .replace(/<@!?(\d+)>/g, (_, p) => `@${getUser(p).username}`)
+      .replace(/<#(\d+)>/g, (_, p) => `#${getChannel(p).name}`)
+      .replace(/<@&(\d+)>/g, (_, p) => `@${guild.roles[p].name}`);
+  }
 };
