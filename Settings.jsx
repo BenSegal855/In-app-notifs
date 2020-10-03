ed416241220257a7a24fb79902caba5542c1c968
BenSegal855 <34194692+BenSegal855@users.noreply.github.com>
2020-10-02 23:02:03 -0400

    Fixed the slider (#16)
    * Fixed typos, grammar, etc (#14)
    
    * Create pull_request_template.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update pull_request_template.md
    
    * Update pull_request_template.md
    
    * Update Settings.jsx
    
    * Update CONTRIBUTING.md
    
    * Update README.md
    
    * Update Settings.jsx
    
    Co-authored-by: BenSegal855 <34194692+BenSegal855@users.noreply.github.com>
    
    * Started adding slider
    
    * Added display time multiplier
    
    * Version bump
    
    * so numbers that arent whole are a thing now
    
    * spaceing
    
    Co-authored-by: saint <58063083+subitize@users.noreply.github.com>

--- Settings.jsx
+++ Settings.jsx
@@ -2,46 +2,45 @@ const { SwitchItem, SliderInput } = require("powercord/components/settings");
 const { React } = require("powercord/webpack");
 
 module.exports = class Settings extends React.Component {
-render() {
-    const { getSetting, toggleSetting } = this.props;
+    render() {
+        const { getSetting, toggleSetting } = this.props;
+        return (
+            <div>
+                <SwitchItem
+                    note="When enabled, notify when pinged."
+                    value={getSetting("notifyPing", false)}
+                    onChange={() => toggleSetting("notifyPing")}
+                >
+                Only notify on ping
+                </SwitchItem>
 
-    return (
-        <div>
-            <SwitchItem
-                note="When enabled, notify when pinged."
-                value={getSetting("notifyPing", false)}
-                onChange={() => toggleSetting("notifyPing")}
-            >
-            Only notify on ping
-            </SwitchItem>
+                <SwitchItem
+                    note="When enabled, desktop notifications will be disabled; if Discord is active."
+                    value={getSetting("blockDesktop", false)}
+                    onChange={() => toggleSetting("blockDesktop")}
+                >
+                Disable desktop notifications when Discord is active
+                </SwitchItem>
 
-            <SwitchItem
-                note="When enabled, desktop notifications will be disabled; if Discord is active."
-                value={getSetting("blockDesktop", false)}
-                onChange={() => toggleSetting("blockDesktop")}
-            >
-            Disable desktop notifications when Discord is active
-            </SwitchItem>
-
-            <SwitchItem
-                note="When enabled, notifications will not vanish automatically."
-                value={getSetting("sticky", false)}
-                onChange={() => toggleSetting("sticky")}
-            >
-            Sticky Notifications
-            </SwitchItem>
-            <SliderInput
-                stickToMarkers
-                minValue={ 1 }
-                maxValue={ 5 }
-                initialValue={ this.props.getSetting("timeMult", 1) }
-                markers={[ 1, 1.25, 1.5, 2, 2.5, 3, 4, 5 ]}
-                className="ianDispTimeMult"
-                defaultValue={ this.props.getSetting("timeMult", 1) }
-                onValueChange={ v => this.props.updateSetting("timeMult", Math.round(v)) }
-                disabled={getSetting("sticky", false)}
-            >Display time multiplier</SliderInput>
-        </div>
-    );
-}
+                <SwitchItem
+                    note="When enabled, notifications will not vanish automatically."
+                    value={getSetting("sticky", false)}
+                    onChange={() => toggleSetting("sticky")}
+                >
+                Sticky Notifications
+                </SwitchItem>
+                <SliderInput
+                    stickToMarkers
+                    minValue={ 1 }
+                    maxValue={ 5 }
+                    initialValue={ this.props.getSetting("timeMult", 1) }
+                    markers={[ 1, 1.25, 1.5, 2, 2.5, 3, 4, 5 ]}
+                    className="ianDispTimeMult"
+                    defaultValue={ this.props.getSetting("timeMult", 1) }
+                    onValueChange={ v => this.props.updateSetting("timeMult", v) }
+                    disabled={getSetting("sticky", false)}
+                >Display time multiplier</SliderInput>
+            </div>
+        );
+    }
 };
