5701f295ef2bf6b2265000458b0a139471b61681
BenSegal855 <bensegal@udel.edu>
2020-11-20 12:10:13 -0500

    some stufs

--- Settings.jsx
+++ Settings.jsx
@@ -1,31 +1,33 @@
-const { SwitchItem, SliderInput } = require("powercord/components/settings");
-const { React } = require("powercord/webpack");
+/* eslint indent: 1*/
+
+const { SwitchItem, SliderInput } = require('powercord/components/settings');
+const { React } = require('powercord/webpack');
 
 module.exports = class Settings extends React.Component {
-    render() {
+    render () {
         const { getSetting, toggleSetting } = this.props;
         return (
             <div>
                 <SwitchItem
                     note="When enabled, notify when pinged."
-                    value={getSetting("notifyPing", false)}
-                    onChange={() => toggleSetting("notifyPing")}
+                    value={getSetting('notifyPing', false)}
+                    onChange={() => toggleSetting('notifyPing')}
                 >
                 Only notify on ping
                 </SwitchItem>
 
                 <SwitchItem
                     note="When enabled, desktop notifications will be disabled; if Discord is active."
-                    value={getSetting("blockDesktop", false)}
-                    onChange={() => toggleSetting("blockDesktop")}
+                    value={getSetting('blockDesktop', false)}
+                    onChange={() => toggleSetting('blockDesktop')}
                 >
                 Disable desktop notifications when Discord is active
                 </SwitchItem>
 
                 <SwitchItem
                     note="When enabled, notifications will not vanish automatically."
-                    value={getSetting("sticky", false)}
-                    onChange={() => toggleSetting("sticky")}
+                    value={getSetting('sticky', false)}
+                    onChange={() => toggleSetting('sticky')}
                 >
                 Sticky Notifications
                 </SwitchItem>
@@ -33,12 +35,12 @@ module.exports = class Settings extends React.Component {
                     stickToMarkers
                     minValue={ 1 }
                     maxValue={ 5 }
-                    initialValue={ this.props.getSetting("timeMult", 1) }
+                    initialValue={ this.props.getSetting('timeMult', 1) }
                     markers={[ 1, 1.25, 1.5, 2, 2.5, 3, 4, 5 ]}
                     className="ianDispTimeMult"
-                    defaultValue={ this.props.getSetting("timeMult", 1) }
-                    onValueChange={ v => this.props.updateSetting("timeMult", v) }
-                    disabled={getSetting("sticky", false)}
+                    defaultValue={ this.props.getSetting('timeMult', 1) }
+                    onValueChange={ v => this.props.updateSetting('timeMult', v) }
+                    disabled={getSetting('sticky', false)}
                 >Display time multiplier</SliderInput>
             </div>
         );
--- index.js
+++ index.js
@@ -1,37 +1,32 @@
+/* eslint indent: 1*/
+
 const { getModule, React, FluxDispatcher } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 const { Plugin } = require('powercord/entities');
-const shouldDisplayNotifications = getModule([ 'shouldDisplayNotifications' ], false);
-const parser = getModule([ 'parse', 'parseTopic' ], false).parse;
+const shouldDisplayNotifications = getModule(['shouldDisplayNotifications'], false);
+const parser = getModule(['parse', 'parseTopic'], false).parse;
 const MessageContent = getModule(m => m.type && m.type.displayName === 'MessageContent', false);
 const Settings = require('./Settings');
 
 module.exports = class InAppNotifciations extends Plugin {
-  async startPlugin () {
-    powercord.api.settings.registerSettings('ian-settings', {
-      category: this.entityID,
-      label: 'In App Notifications',
-      render: Settings
-    });
+    async startPlugin() {
+        powercord.api.settings.registerSettings('ian-settings', {
+            category: this.entityID,
+            label: 'In App Notifications',
+            render: Settings
+        });
 
-    FluxDispatcher.subscribe('MESSAGE_CREATE', (msg) => {
-      if (this.shouldNotify(msg.message)) {
-        console.log(msg);
-      }
-    });
-  }
+        this.messageGot = this.messageGot.bind(this);
+        FluxDispatcher.subscribe('MESSAGE_CREATE', this.messageGot);
+    }
 
-  pluginWillUnload () {
-    /*
-     * uninject('ian');
-     * uninject('ian-desktop-blocker');
-     */
-    FluxDispatcher.unsubscribe('MESSAGE_CREATE');
-    powercord.api.settings.unregisterSettings('ian-settings');
-  }
+    pluginWillUnload() {
+        FluxDispatcher.unsubscribe('MESSAGE_CREATE', this.messageGot);
+        powercord.api.settings.unregisterSettings('ian-settings');
+    }
 
-  shouldNotify (msg) {
-    // const user = getModule([ 'getCurrentUser' ]).getCurrentUser();
-    return msg.author.id === '465668689920917534';
-  }
+    messageGot(msg) {
+        // const user = getModule([ 'getCurrentUser' ]).getCurrentUser();
+        console.log('got a message 2');
+    }
 };
